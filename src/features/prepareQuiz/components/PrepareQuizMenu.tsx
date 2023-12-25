0a7e789a45bf25a92ba11e22fbe420c1f6a4fb83
otohusan <tamioguitar@gmail.com>
2023-12-25 16:42:03 +0900

    feat: prepareにplayページに飛ぶボタンを追加

--- src/features/prepareQuiz/components/PrepareQuizMenu.tsx
+++ src/features/prepareQuiz/components/PrepareQuizMenu.tsx
@@ -1,17 +1,22 @@
 import "../style/PrepareMenus.css";
 import SpeakWordBtn from "./SpeakWordBtn";
+import { FaPlay } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 
 type PrepareQuizMenuProps = {
   QuizName: string;
 };
 
 function PrepareQuizMenu({ QuizName }: PrepareQuizMenuProps) {
+  const navigate = useNavigate();
   return (
     <div className="PrepareMenus">
       <div className="PrepareQuizSpeakBtn">
         <SpeakWordBtn questionWord={QuizName} />
       </div>
-      <div className="PrepareQuizMenu">...</div>
+      <div className="PrepareQuizPlayBtn" onClick={() => navigate("/")}>
+        <FaPlay />
+      </div>
     </div>
   );
 }
