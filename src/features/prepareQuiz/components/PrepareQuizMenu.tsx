85c7214ee6a503f308387cc88bbb588eb461efa8
otohusan <tamioguitar@gmail.com>
2023-12-25 17:03:57 +0900

    feat: prepareにshareボタンを追加

--- src/features/prepareQuiz/components/PrepareQuizMenu.tsx
+++ src/features/prepareQuiz/components/PrepareQuizMenu.tsx
@@ -1,6 +1,7 @@
 import "../style/PrepareMenus.css";
 import SpeakWordBtn from "./SpeakWordBtn";
 import { FaPlay } from "react-icons/fa6";
+import { MdIosShare } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 
 type PrepareQuizMenuProps = {
@@ -9,13 +10,18 @@ type PrepareQuizMenuProps = {
 
 function PrepareQuizMenu({ QuizName }: PrepareQuizMenuProps) {
   const navigate = useNavigate();
+  const iconSize: string = "3vw";
   return (
     <div className="PrepareMenus">
       <div className="PrepareQuizSpeakBtn">
         <SpeakWordBtn questionWord={QuizName} />
       </div>
       <div className="PrepareQuizPlayBtn" onClick={() => navigate("/")}>
-        <FaPlay />
+        {/* 大きさ整えるためにサイズ指定している */}
+        <FaPlay size={iconSize} />
+      </div>
+      <div>
+        <MdIosShare size={iconSize} />
       </div>
     </div>
   );
