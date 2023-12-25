6c3309b3242a9101e9a9734e8fc361e1c261d7f0
otohusan <tamioguitar@gmail.com>
2023-12-25 20:27:05 +0900

    style: styleの微調整

--- src/features/playQuiz/components/GoPrepareBtn.tsx
+++ src/features/playQuiz/components/GoPrepareBtn.tsx
@@ -12,7 +12,7 @@ function GoPrepareBtn(): JSX.Element {
   return (
     <div className="playQuiz-GoPrepareBtn" onClick={handleClick}>
       <FaRegLightbulb
-        size={35}
+        size={30}
         style={{
           filter:
             "drop-shadow(0px 1px 0px #f09038) drop-shadow(2px 0px 2px rgba(0, 0, 0, 0.5)",
--- src/features/playQuiz/components/GoReviewBtn.tsx
+++ src/features/playQuiz/components/GoReviewBtn.tsx
@@ -12,7 +12,7 @@ function GoReviewBtn(): JSX.Element {
   return (
     <div className="playQuiz-GoReviewBtn" onClick={handleClick}>
       <GoNote
-        size={35}
+        size={30}
         style={{
           filter:
             "drop-shadow(0px 1px 0px #f09038) drop-shadow(2px 0px 2px rgba(0, 0, 0, 0.5)",
