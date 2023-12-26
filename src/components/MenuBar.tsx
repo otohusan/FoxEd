28f4b9681c4069b8213c76b0e76d1c55351188e0
otohusan <tamioguitar@gmail.com>
2023-12-26 14:36:33 +0900

    style: 背景画像無くして、styleの微調整

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -50,12 +50,12 @@ function MenuBar() {
       {isOpen && (
         <div className="MenuContainer">
           <div className="MenuContent" ref={menuRef}>
-            <div onClick={() => navigate("/")}>クイズをプレイ ⭕️❌</div>
-            <div onClick={() => navigate("/PrepareQuiz")}>単語を覚える 💡</div>
-            <div onClick={() => navigate("/ReviewQuiz")}>単語を復習 📝</div>
             <div onClick={() => navigate("/ChooseQuizData")}>
               単語データを選択 🔍
             </div>
+            <div onClick={() => navigate("/")}>クイズをプレイ ⭕️❌</div>
+            <div onClick={() => navigate("/PrepareQuiz")}>単語を覚える 💡</div>
+            <div onClick={() => navigate("/ReviewQuiz")}>単語を復習 📝</div>
             <div onClick={toggleMenu}>メニューを閉じる</div>
           </div>
         </div>
--- src/features/chooseQuiz/components/ChooseQuiz.tsx
+++ src/features/chooseQuiz/components/ChooseQuiz.tsx
@@ -1,7 +1,7 @@
 import { useNavigate, NavigateFunction } from "react-router-dom";
 import { Header } from "../../../components";
 import ChooseQuizContainer from "./ChooseQuizContainer";
-import BacPic from "../../../assets/BacPic.png";
+// import BacPic from "../../../assets/BacPic.png";
 import "../style/WalkGirl.css";
 
 type Quiz = {
@@ -36,7 +36,7 @@ function ChooseQuiz({ quizzes, setQuizzes }: ChooseQuizProps) {
   return (
     <div>
       <Header HeaderTitle="Choose" />
-      <img src={BacPic} className="WalkGirl" />
+      {/* <img src={BacPic} className="WalkGirl" /> */}
       <div style={{ color: "black", marginTop: "25%" }}>
         {quizzes.map((quizFormat, index) => (
           <div
