37b5a11f979b98f8ee414d2797bfb46af3765419
otohusan <tamioguitar@gmail.com>
2024-01-08 19:26:15 +0900

    feat: reviewQuiz用のtypeを作成

--- src/assets/reviewQuizzes.ts
+++ src/assets/reviewQuizzes.ts
@@ -1,1 +1,9 @@
 export const reviewQuizzesIndex: number[] = [];
+
+export type reviewQuiz = {
+  question: string;
+  answer: string;
+  partOfSpeech: number;
+};
+
+export const reviewQuizzes: reviewQuiz[] = [];
