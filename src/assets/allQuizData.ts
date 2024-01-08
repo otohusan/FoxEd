066ef9c9f80e583332b0b262fc1a127153e36dbe
otohusan <tamioguitar@gmail.com>
2024-01-08 12:13:15 +0900

    chore: 単語データの追加

--- src/assets/allQuizData.ts
+++ src/assets/allQuizData.ts
@@ -1,5 +1,6 @@
 import { quizData1 } from "./quizData1";
 import { quizData2 } from "./quizData2";
+import { toeicStandard } from "./toeicStandard.ts";
 import { QuizFormat } from "../../type/index.ts";
 
-export const allQuizzes: QuizFormat[] = [quizData1, quizData2];
+export const allQuizzes: QuizFormat[] = [toeicStandard, quizData1, quizData2];
