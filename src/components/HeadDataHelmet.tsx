61ae2e55ae40de37e0109452e991f2c209c378a4
otohusan <tamioguitar@gmail.com>
2024-01-30 16:53:10 +0900

    style: chooseの見た目を改修

--- src/components/HeadDataHelmet.tsx
+++ src/components/HeadDataHelmet.tsx
@@ -2,20 +2,34 @@ import { Helmet } from "react-helmet";
 
 type HeadDataHelmetProps = {
   pageTitle: string;
+  pageDescription?: string;
 };
 
-const HeadDataHelmet = ({ pageTitle }: HeadDataHelmetProps) => {
+const HeadDataHelmet = ({
+  pageTitle,
+  pageDescription,
+}: HeadDataHelmetProps) => {
   return (
-    <>
+    <div>
       <Helmet>
         <title>{`${pageTitle} - Konwalk`}</title>
+        <meta
+          name="description"
+          content={
+            pageDescription ||
+            "授業中に宿題をしたように、歩く時間も有効に使う。その手助けを行う英単語帳 | 高校英語・TOEIC対応"
+          }
+        />
         <meta
           property="og:title"
           content="Konwalk(コンウォーク) - 歩く時間を無駄にしない英単語帳"
         />
         <meta
           property="og:description"
-          content="授業中に宿題をしたように、歩く時間も有効に使う。その手助けを行う英単語帳 | 高校英語・TOEIC対応"
+          content={
+            pageDescription ||
+            "授業中に宿題をしたように、歩く時間も有効に使う。その手助けを行う英単語帳 | 高校英語・TOEIC対応"
+          }
         />
         <meta property="og:site_name" content="Konwalk" />
         <meta
@@ -23,7 +37,7 @@ const HeadDataHelmet = ({ pageTitle }: HeadDataHelmetProps) => {
           content={import.meta.env.VITE_IMAGE_WITH_SHARE_URL}
         />
       </Helmet>
-    </>
+    </div>
   );
 };
 
