3aeb9aa58cf9947e3933b795d4a5c988d406f9a7
otohusan <tamioguitar@gmail.com>
2024-02-02 20:19:14 +0900

    chore: package.jsonの追加

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -11,72 +11,6 @@ export function render(url: string | Partial<Location<any>>) {
   );
 }
 
-// // server.js
-// import express from "express";
-// import fs from 'fs'
-// import path from 'path'
-// import { fileURLToPath } from 'url'
-// import express from 'express'
-// import { createServer as createViteServer } from 'vite'
-
-// const server = express();
-
-// const __dirname = path.dirname(new URL(import.meta.url).pathname);
-
-// const clientDistPath = path.join(__dirname, "../../dist/client");
-// // メインのJavaScriptファイル名を取得
-// const mainJsFileName = fs
-//   .readdirSync(clientDistPath)
-//   .find((file) => file.startsWith("main") && file.endsWith(".js"));
-
-// // 静的ファイルを提供
-// server.use(express.static(clientDistPath));
-
-// server.get("*", (req, res) => {
-//   const appHtml = render(req.url);
-//   const html = `
-//   <!DOCTYPE html>
-//   <html lang="ja">
-//     <head prefix="og: https://ogp.me/ns#">
-//       <meta charset="UTF-8" />
-//       <title>Konwalk</title>
-//       <meta
-//         name="description"
-//         content="授業中に宿題をしたように、歩く時間も有効に使う。その手助けを行う英単語帳 | 高校英語・TOEIC対応"
-//       />
-//       <link rel="icon" type="image/svg+xml" href="/Konwalk_favicon.svg" />
-//       <link
-//         rel="stylesheet"
-//         href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap"
-//       />
-//       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-//       <meta
-//         property="og:title"
-//         content="Konwalk(コンウォーク) - 歩く時間を無駄にしない英単語帳"
-//       />
-//       <meta property="og:type" content="website" />
-//       <meta property="og:url" content="https://Konwalk.jp" />
-//       <meta property="og:site_name" content="Konwalk" />
-//       <meta
-//         property="og:description"
-//         content="授業中に宿題をしたように、歩く時間も有効に使う! その手助けを行う英単語帳Konwalk(コンウォーク)"
-//       />
-//       <meta property="og:image" content="%VITE_IMAGE_WITH_SHARE_URL%" />
-//     </head>
-//     <body>
-//       <div id="root"><!--outlet-->${appHtml}</div>
-//       <script type="module" src="/dist/assets/${mainJsFileName}"></script>
-//     </body>
-//   </html>
-//   `;
-//   res.send(html);
-// });
-
-// const PORT = process.env.PORT || 8787;
-// server.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
-
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -107,8 +41,9 @@ async function createServer() {
   // 次のコードは再起動後でも有効です。
   app.use(vite.middlewares);
 
-  app.use("*", async (req, res, next) => {
-    const url = req.originalUrl;
+  //TODO: 型定義をする
+  app.use("*", async (request: any, reply: any, next: any) => {
+    const url = request.originalUrl;
 
     try {
       // 1. index.html を読み込む
@@ -131,7 +66,7 @@ async function createServer() {
       const html = template.replace(`<!--outlet-->`, appHtml);
 
       // 6. レンダリングされた HTML をクライアントに送ります。
-      res.status(200).set({ "Content-Type": "text/html" }).end(html);
+      reply.status(200).set({ "Content-Type": "text/html" }).end(html);
     } catch (e: any) {
       // エラーが検出された場合は、Vite にスタックトレースを修正させ、実際のソースコードに
       // マップし直します。
@@ -140,7 +75,9 @@ async function createServer() {
     }
   });
 
-  app.listen(8787);
+  app.listen({ port: 8787 }, () => {
+    console.log("server is running");
+  });
 }
 
 createServer();
