613aeb514e11b6d1030b21df39ec098a5329e588
otohusan <tamioguitar@gmail.com>
2024-03-29 22:25:25 +0900

    feat: 紹介に文章を追加

--- src/features/chooseQuiz/introduction/Introduction.tsx
+++ src/features/chooseQuiz/introduction/Introduction.tsx
@@ -1,7 +1,12 @@
 import "./style/Introduction.css";
 
 function Introduction() {
-  return <div className="introductionContainer">Introduction</div>;
+  return (
+    <div className="introductionContainer">
+      <h1>{`Konwalk(コンウォーク)を\n知ってくれて\nありがとう!`}</h1>
+      <h2>{`Konwalk(コンウォーク)は\n歩く時間を、勉強ができる時間に\n変える単語帳だよ`}</h2>
+    </div>
+  );
 }
 
 export default Introduction;
