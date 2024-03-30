1040ed9ec1bcc8a667e5364bd03111b59a6cc34a
otohusan <tamioguitar@gmail.com>
2024-03-30 12:39:02 +0900

    style: 紹介のスタイル整え

--- src/features/chooseQuiz/introduction/Introduction.tsx
+++ src/features/chooseQuiz/introduction/Introduction.tsx
@@ -3,8 +3,19 @@ import "./style/Introduction.css";
 function Introduction() {
   return (
     <div className="introductionContainer">
-      <h1>{`Konwalk(コンウォーク)を\n知ってくれて\nありがとう!`}</h1>
-      <h2>{`Konwalk(コンウォーク)は\n歩く時間を、勉強ができる時間に\n変える単語帳だよ`}</h2>
+      <h1>
+        <span className="introductionBig">Konwalk(コンウォーク)</span>
+        を<br />
+        知ってくれて
+        <br />
+        <span className="introductionBold">ありがとう!</span>
+      </h1>
+      <h2>
+        <span className="introductionBold">下の単語データ</span>から
+        <br />
+        覚えたい単語を選んでね
+      </h2>
+      {/* <h2>{`Konwalk(コンウォーク)は\n歩く時間を、勉強ができる時間に\n変える単語帳だよ`}</h2> */}
     </div>
   );
 }
