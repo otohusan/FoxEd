8069539f06daa8edf821678fc0d548d22238a6e1
otohusan <tamioguitar@gmail.com>
2024-04-05 21:09:25 +0900

    feat: chooseにplayとprepareを選択できるように

--- src/features/chooseQuiz/components/SelectQuizModeContainer.tsx
+++ src/features/chooseQuiz/components/SelectQuizModeContainer.tsx
@@ -1,15 +1,23 @@
 import { Link } from "react-router-dom";
 import "../style/SelectQuizMode.css";
 
-function SelectQuizModeContainer() {
+type SelectQuizModeContainerProps = {
+  x: number;
+  y: number;
+};
+
+function SelectQuizModeContainer(props: SelectQuizModeContainerProps) {
   return (
-    <div className="SelectQuizModeContainer">
-      <Link to={"/"} className="SelectMode">
-        歩いて使う
+    <div
+      className="SelectQuizModeContainer"
+      style={{ top: `${props.y}px`, left: `${props.x}px` }}
+    >
+      <Link to={"/PlayQuiz"} className="SelectMode">
+        歩いて覚える
       </Link>
       <div className="SelectMode-hr-line"></div>
-      <Link to={"/"} className="SelectMode">
-        普通に使う
+      <Link to={"/PrepareQuiz"} className="SelectMode">
+        単語帳で覚える
       </Link>
     </div>
   );
