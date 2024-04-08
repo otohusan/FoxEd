42b653e49f97d614eb6c1330cb13b058d02f741a
otohusan <tamioguitar@gmail.com>
2024-04-08 12:31:42 +0900

    chore: chooseと共通コンポーネントの不要ファイルを削除

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,50 +2,6 @@ import { useEffect, useState } from "react";
 import "./style/Header.css";
 import { MenuBar } from ".";
 
-// windowに依存してるやつ
-// function Header({ HeaderTitle }: { HeaderTitle: string }): JSX.Element {
-//   const [isVisible, setIsVisible] = useState(true);
-//   const [lastScrollY, setLastScrollY] = useState(window.scrollY);
-//   const threshold = 50;
-
-//   useEffect(() => {
-//     const handleScroll = () => {
-//       let currentScrollY = window.scrollY;
-//       const pageHeight = document.body.scrollHeight;
-//       const viewportHeight = window.innerHeight;
-//       const bottomPosition = pageHeight - viewportHeight;
-
-//       if (Math.abs(currentScrollY - lastScrollY) > threshold) {
-//         // ページの最下部近くでのスクロールでは状態を変更しない
-//         if (currentScrollY >= bottomPosition - threshold) {
-//           return;
-//         }
-
-//         if (currentScrollY < lastScrollY || currentScrollY < 10) {
-//           setIsVisible(true);
-//         } else {
-//           setIsVisible(false);
-//         }
-
-//         setLastScrollY(currentScrollY);
-//       }
-//     };
-
-//     // window.addEventListener("scroll", handleScroll);
-//     // return () => window.removeEventListener("scroll", handleScroll);
-//     useScroll(handleScroll);
-//   }, [lastScrollY]);
-
-//   return (
-//     <div className={`HeaderContainer ${!isVisible ? "hidden" : ""}`}>
-//       <div className="HeaderTitle">{HeaderTitle} </div>
-//       <MenuBar />
-//     </div>
-//   );
-// }
-
-// export default Header;
-
 type HeaderProps = {
   HeaderTitle: string;
 };
