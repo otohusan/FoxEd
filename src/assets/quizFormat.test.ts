b72ee5b879635204adbe1e3a97d62b01e23f71a8
otohusan <tamioguitar@gmail.com>
2024-04-15 14:08:20 +0900

    test: 適切な答えと選択肢の関係性があるかチェックするテストの追加

--- src/assets/quizFormat.test.ts
+++ src/assets/quizFormat.test.ts
@@ -2,10 +2,11 @@ import { describe, it, expect } from "vitest";
 import { quizzes } from "./quizzes";
 import { allQuizzes } from "./allQuizData";
 
+const quizCategoryList = [quizzes, allQuizzes];
+
 describe("QuizFormat", () => {
-  it("should not have duplicate questions", () => {
+  it("単語データに被りが無いか確認", () => {
     let someCondition = true; // この条件を動的に変更してテストの挙動を確認
-    const quizCategoryList = [quizzes, allQuizzes];
 
     quizCategoryList.map((quizCategory) => {
       const questions: string[] = [];
@@ -26,6 +27,34 @@ describe("QuizFormat", () => {
             // 序盤に失敗すると、それ以降にサイズに差が生まれて全てエラーになることを防ぐために乱数をsetに追加してる
             const randomNumber = Math.random().toString();
             uniqueQuestions.add(randomNumber);
+          }
+        });
+      });
+    });
+
+    expect(someCondition).toBe(true);
+  });
+
+  it("問題の答えが選択肢に含まれているか", () => {
+    let someCondition = true; // この条件を動的に変更してテストの挙動を確認
+
+    quizCategoryList.map((quizCategory) => {
+      quizCategory.map((quizFormat) => {
+        quizFormat.body.map((quiz) => {
+          // 選択肢と答えが適切であるかを判定結果を格納する変数
+          let isIncludedOrNot = false;
+          // 判定
+          quiz.choices.map((choice) => {
+            if (choice === quiz.answer) {
+              isIncludedOrNot = true;
+            }
+          });
+          // 適切でない場合の処理
+          if (!isIncludedOrNot) {
+            someCondition = false;
+            console.log(
+              `${quizFormat.label}の${quiz.question}に適切な答えor選択肢が含まれていない`
+            );
           }
         });
       });
