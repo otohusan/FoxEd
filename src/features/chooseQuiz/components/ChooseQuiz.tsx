714b37d25793c9860572fb2241dc6a16404d279c
otohusan <tamioguitar@gmail.com>
2024-04-27 11:08:07 +0900

    feat: 選択画面に戻ったときに画面上部からスタートするように

--- src/features/chooseQuiz/components/ChooseQuiz.tsx
+++ src/features/chooseQuiz/components/ChooseQuiz.tsx
@@ -1,7 +1,5 @@
-// import { useEffect } from "react";
 import { Header, Footer, HeadDataHelmet } from "../../../components";
 import ChooseQuizContainer from "./ChooseQuizContainer";
-// import BacPic from "../../../assets/BacPic.png";
 import "../style/WalkGirl.css";
 import "../style/ChooseQuizContainer.css";
 import { QuizFormat } from "../../../../type/index.ts";
@@ -24,9 +22,9 @@ function labelOnClick(
 }
 
 function ChooseQuiz({ quizzes, setQuizzes }: ChooseQuizProps) {
-  // useEffect(() => {
-  //   window.scrollTo(0, 0);
-  // }, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   const [isSelectModeOpen, setIsSelectModeOpen] = useState(false);
   // selectModeコンポーネントの位置を設定するステート（例）
