0c77a5e05364d270ed90901f7ba5bba6ea017e21
otohusan <tamioguitar@gmail.com>
2024-05-05 11:13:13 +0900

    feat: runnnnigManが飛ぶように

--- src/features/playQuiz/components/dpad/GamePad.tsx
+++ src/features/playQuiz/components/dpad/GamePad.tsx
@@ -4,11 +4,40 @@ import { FaRegLightbulb } from "react-icons/fa6";
 import { GoHome } from "react-icons/go";
 import { GoNote } from "react-icons/go";
 import { IoVideocamOutline } from "react-icons/io5";
+import { FaRunning } from "react-icons/fa";
 
 function GamePad() {
+  function moveRunningManRight() {
+    const runningMan = document.getElementById("runningMan");
+    if (!runningMan) return; // 要素がない場合は何もしない
+
+    // getComputedStyleを使って現在のleftの値を取得し、整数値に変換
+    const currentLeftPosition = parseInt(
+      window.getComputedStyle(runningMan).left,
+      10
+    );
+    const currentTopPosition = parseInt(
+      window.getComputedStyle(runningMan).top,
+      10
+    );
+    const newLeftPosition = currentLeftPosition + 30; // 右に10ピクセル移動
+    const newTopPosition = currentTopPosition - 100; // 右に10ピクセル移動
+
+    runningMan.style.left = newLeftPosition + "px"; // 新しい位置を設定
+    runningMan.style.top = newTopPosition + "px"; // 新しい位置を設定
+
+    setTimeout(() => {
+      runningMan.style.top = currentTopPosition + "px";
+      runningMan.style.left = newLeftPosition + 10 + "px";
+    }, 200);
+  }
   return (
     <div className="GamepadContainer">
-      <div className="GamePlayView"></div>
+      <div className="GamePlayView">
+        <div className="runningMan" id="runningMan">
+          <FaRunning size={40} onClick={moveRunningManRight} />
+        </div>
+      </div>
       <button className="gamepadPiconBtn">
         <FaRegLightbulb size={20} />
       </button>
