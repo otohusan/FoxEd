280296dafdd635bf74a5a0576691735d592261ee
otohusan <tamioguitar@gmail.com>
2024-05-05 12:27:44 +0900

    feat: runnningManにパラパラ漫画のような走るアニメーションを追加

--- src/features/playQuiz/components/dpad/GamePad.tsx
+++ src/features/playQuiz/components/dpad/GamePad.tsx
@@ -4,7 +4,10 @@ import { FaRegLightbulb } from "react-icons/fa6";
 import { GoHome } from "react-icons/go";
 import { GoNote } from "react-icons/go";
 import { IoVideocamOutline } from "react-icons/io5";
-import { FaRunning } from "react-icons/fa";
+import { RiRunLine } from "react-icons/ri";
+import { GrRun } from "react-icons/gr";
+import { BiRun } from "react-icons/bi";
+import { useEffect, useState } from "react";
 
 function GamePad() {
   function moveRunningManRight() {
@@ -21,21 +24,66 @@ function GamePad() {
       10
     );
     const newLeftPosition = currentLeftPosition + 30; // 右に10ピクセル移動
-    const newTopPosition = currentTopPosition - 100; // 右に10ピクセル移動
+    const newTopPosition = currentTopPosition - 140; // 右に10ピクセル移動
 
     runningMan.style.left = newLeftPosition + "px"; // 新しい位置を設定
     runningMan.style.top = newTopPosition + "px"; // 新しい位置を設定
 
     setTimeout(() => {
-      runningMan.style.top = currentTopPosition + "px";
       runningMan.style.left = newLeftPosition + 10 + "px";
+      runningMan.style.top = currentTopPosition + "px";
     }, 200);
   }
+
+  // パラパラ漫画用の変数
+  let currentFrame = 0;
+  const [animeFlag, setAnimeFlag] = useState(false);
+
+  function animateRunning() {
+    const frames = document.querySelectorAll(
+      ".runningMan > *"
+    ) as NodeListOf<HTMLElement>;
+
+    frames.forEach((frame) => {
+      frame.style.display = "none"; // 全てのフレームを非表示に
+    });
+
+    frames[currentFrame].style.display = "block";
+
+    currentFrame = (currentFrame + 1) % frames.length; // 次のフレームに移動
+  }
+
+  // アニメーションを開始
+  useEffect(() => {
+    let intervalId: string | number | NodeJS.Timeout | undefined;
+
+    if (animeFlag) {
+      // animeFlagがtrueの場合のみアニメーションを開始
+      intervalId = setInterval(animateRunning, 300);
+    } else {
+      // animeFlagがfalseの場合には既存のインターバルをクリア
+      clearInterval(intervalId);
+    }
+    // コンポーネントのアンマウント時にインターバルをクリアする
+    return () => clearInterval(intervalId);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [animeFlag]);
+
   return (
     <div className="GamepadContainer">
       <div className="GamePlayView">
         <div className="runningMan" id="runningMan">
-          <FaRunning size={40} onClick={moveRunningManRight} />
+          <RiRunLine size={40} onClick={moveRunningManRight} />
+          <BiRun
+            size={40}
+            onClick={moveRunningManRight}
+            style={{ display: "none" }}
+          />
+          <GrRun
+            size={40}
+            onClick={moveRunningManRight}
+            style={{ display: "none" }}
+          />
         </div>
       </div>
       <button className="gamepadPiconBtn">
@@ -49,7 +97,12 @@ function GamePad() {
         <GoNote size={20} />
       </button>
 
-      <button className="gamepadVideoBtn">
+      <button
+        className="gamepadVideoBtn"
+        onClick={() => {
+          setAnimeFlag(!animeFlag);
+        }}
+      >
         <IoVideocamOutline size={20} />
       </button>
 
