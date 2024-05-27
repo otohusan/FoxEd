f575c53792d9e743c5e0ad2918e3632cb2558d86
otohusan <tamioguitar@gmail.com>
2024-05-27 09:37:22 +0900

    feat: 要素が画面内に表示されたか探知するフックを作成

--- src/components/QuizCard.tsx
+++ src/components/QuizCard.tsx
@@ -1,10 +1,45 @@
-import { useState } from "react";
 import "./style/quizCard.css";
+import { useEffect, useRef, useState } from "react";
+
+type Options = {
+  root?: Element | null;
+  rootMargin?: string;
+  threshold?: number | number[];
+};
+
+const useElementOnScreen = (
+  options: Options
+): [React.RefObject<HTMLDivElement>, boolean] => {
+  const containerRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const callbackFunction = (entries: IntersectionObserverEntry[]) => {
+    const [entry] = entries;
+    setIsVisible(entry.isIntersecting);
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(callbackFunction, options);
+    const currentRef = containerRef.current;
+    if (currentRef) observer.observe(currentRef);
+
+    return () => {
+      if (currentRef) observer.unobserve(currentRef);
+    };
+  }, [containerRef, options]);
+
+  return [containerRef, isVisible];
+};
 
 type quizCardProps = { frontElement: string; backElement: string };
 
 const QuizCard = (props: quizCardProps) => {
   const [isFlipped, setIsFlipped] = useState(false);
+  const [containerRef, isVisible] = useElementOnScreen({
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.85,
+  });
 
   const handleClick = () => {
     setIsFlipped(!isFlipped);
@@ -12,12 +47,18 @@ const QuizCard = (props: quizCardProps) => {
 
   return (
     <div
-      className={`quiz-card ${isFlipped ? "is-flipped" : ""}`}
+      className={`quiz-card ${isFlipped ? "is-flipped" : ""} ${
+        isVisible ? "is-visible" : ""
+      }`}
       onClick={handleClick}
     >
-      <div className="quiz-card-inner">
-        <div className="quiz-card-front">{props.frontElement}</div>
-        <div className="quiz-card-back">{props.backElement}</div>
+      <div className="quiz-card-inner" ref={containerRef}>
+        <div className={`quiz-card-front ${isVisible ? "is-visible" : ""}`}>
+          {props.frontElement}
+        </div>
+        <div className={`quiz-card-back ${isVisible ? "is-visible" : ""}`}>
+          {props.backElement}
+        </div>
       </div>
     </div>
   );
