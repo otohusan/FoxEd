7c3183501eb56e17af10eafe6f7bd4b043638663
otohusan <tamioguitar@gmail.com>
2024-05-27 17:03:28 +0900

    style: infoBadgeのcss修正

--- src/components/InfoBadge.tsx
+++ src/components/InfoBadge.tsx
@@ -1,19 +1,11 @@
+import "./style/InfoBadge.css";
+
 type InfoBadgeProps = {
   text: string;
-  color: string;
-  backgroundColor: string;
 };
 
-const InfoBadge = ({ text, color, backgroundColor }: InfoBadgeProps) => {
-  const badgeStyle = {
-    color: color || "white",
-    backgroundColor: backgroundColor || "blue",
-    padding: "5px 10px",
-    borderRadius: "5px",
-    display: "inline-block",
-  };
-
-  return <span style={badgeStyle}>{text}</span>;
+const InfoBadge = ({ text }: InfoBadgeProps) => {
+  return <span className="info-badge">{text}</span>;
 };
 
 export default InfoBadge;
