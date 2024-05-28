dd01e92e81508e8b4224e8b51ea86bd975ffd02a
otohusan <tamioguitar@gmail.com>
2024-05-28 11:16:02 +0900

    feat: ボトムナビゲーションで関数を受け取るように変更

--- src/components/BottomNavigation.tsx
+++ src/components/BottomNavigation.tsx
@@ -1,25 +1,63 @@
-import React from "react";
-import { AiOutlineHome, AiOutlineSearch, AiOutlineUser } from "react-icons/ai";
+import React, { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { GoHome } from "react-icons/go";
+import { CgProfile } from "react-icons/cg";
 import "./style/BottomNavigation.css";
 
 const BottomNavigation = () => {
   const navItems = [
-    { text: "ホーム", link: "#home", icon: <AiOutlineHome /> },
-    { text: "プロフィール", link: "#profile", icon: <AiOutlineUser /> },
-    { text: "検索", link: "#search", icon: <AiOutlineSearch /> },
+    {
+      text: "ホーム",
+      icon: <GoHome />,
+      id: "home",
+      onClick: () => {},
+    },
+    {
+      text: "プロフィール",
+      icon: <CgProfile />,
+      id: "profile",
+      onClick: () => {},
+    },
+    {
+      text: "探す",
+      icon: <AiOutlineSearch />,
+      id: "search",
+      onClick: () => {},
+    },
   ];
 
-  const color = "#585858c";
+  const [selected, setSelected] = useState("home");
+  const selectedIconColor = "#f67a27";
+  const selectedTextColor = "#ff802b";
+
+  const handleClick = (id: string, onClick: () => void) => {
+    setSelected(id);
+    onClick();
+  };
 
   return (
     <nav className="bottom-navigation">
-      {navItems.map((item, index) => (
-        <div key={index} className="nav-item">
-          <a href={item.link} className="nav-link">
+      {navItems.map((item) => (
+        <div
+          key={item.id}
+          className={`nav-item ${selected === item.id ? "is-selected" : ""}`}
+          onClick={() => handleClick(item.id, item.onClick)}
+        >
+          <div className="nav-link">
             {item.icon &&
-              React.cloneElement(item.icon, { className: "nav-icon", color })}
-            <span className="nav-text">{item.text}</span>
-          </a>
+              React.cloneElement(item.icon, {
+                className: "nav-icon",
+                color: selected === item.id ? selectedIconColor : "#838383",
+              })}
+            <span
+              className="nav-text"
+              style={{
+                color: selected === item.id ? selectedTextColor : "#6e6e6e",
+              }}
+            >
+              {item.text}
+            </span>
+          </div>
         </div>
       ))}
     </nav>
