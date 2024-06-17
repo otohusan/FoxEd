6b9be4a5f360e7ad9670bc677172550decca8e8c
otohusan <tamioguitar@gmail.com>
2024-06-17 20:26:51 +0900

    feat: プライバシーポリシーページの作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,3 +7,4 @@ export { default as Login } from "./Login";
 export { default as Profile } from "./Profile";
 export { default as Register } from "./Register";
 export { default as Search } from "./Search";
+export { default as ApplicationPrivacy } from "./ApplicationPrivacy";
