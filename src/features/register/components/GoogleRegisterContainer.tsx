ebcaac4d7ba49bdbc027106f4f3341f91f2aad20
otohusan <tamioguitar@gmail.com>
2024-06-17 17:16:01 +0900

    feat: googleログインを可能に

--- src/features/register/components/GoogleRegisterContainer.tsx
+++ src/features/register/components/GoogleRegisterContainer.tsx
@@ -2,8 +2,21 @@ import { useGoogleLogin } from "@react-oauth/google";
 import { FcGoogle } from "react-icons/fc";
 import "../style/GoogleRegisterContainer.css";
 import axios from "axios";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../../components/auth/useAuth";
+
+type DecodedToken = {
+  userID: string;
+  exp: number;
+  // 他の必要なフィールドがあれば追加
+};
 
 function GoogleRegisterContainer() {
+  const navigate = useNavigate();
+
+  const { setUser } = useAuth();
+
   const VITE_BASE_BACKEND_URL = import.meta.env.VITE_BASE_BACKEND_URL;
   // 取得したアクセストークンをバックエンドに送る
   // resにはJWTトークンが含まれることを想定
@@ -16,7 +29,32 @@ function GoogleRegisterContainer() {
             access_token: tokenResponse.access_token,
           }
         );
-        console.log(response.data);
+        const token = response.data.token; // JWTトークンを取得
+        const decoded: DecodedToken = jwtDecode(token); // JWTトークンをデコード
+
+        const userInfoResponse = await axios.get(
+          `${VITE_BASE_BACKEND_URL}/users/${decoded.userID}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const userInfo = userInfoResponse.data;
+
+        // NOTICE: jsonが大文字で返ってきてる
+        const user = {
+          ID: userInfo.ID, // 正しいキー名を使用
+          name: userInfo.Name,
+          email: userInfo.Email,
+          createdAt: userInfo.CreatedAt,
+        };
+        setUser(user);
+        // 必要ならローカルストレージにトークンを保存
+        localStorage.setItem("token", token);
+
+        // ホームに遷移
+        navigate("/");
       } catch (error) {
         console.error("Failed to login with Google:", error);
       }
