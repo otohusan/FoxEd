d989994b98f9cb13deb77f1d244229def69fd0f5
otohusan <tamioguitar@gmail.com>
2024-06-17 15:47:14 +0900

    feat: Googleから取得したアクセストークンをバックエンドに送るように

--- src/features/register/components/GoogleRegisterContainer.tsx
+++ src/features/register/components/GoogleRegisterContainer.tsx
@@ -1,10 +1,26 @@
 import { useGoogleLogin } from "@react-oauth/google";
 import { FcGoogle } from "react-icons/fc";
 import "../style/GoogleRegisterContainer.css";
+import axios from "axios";
 
 function GoogleRegisterContainer() {
+  const VITE_BASE_BACKEND_URL = import.meta.env.VITE_BASE_BACKEND_URL;
+  // 取得したアクセストークンをバックエンドに送る
+  // resにはJWTトークンが含まれることを想定
   const login = useGoogleLogin({
-    onSuccess: (tokenResponse) => console.log(tokenResponse),
+    onSuccess: async (tokenResponse) => {
+      try {
+        const response = await axios.post(
+          `${VITE_BASE_BACKEND_URL}/auth/google`,
+          {
+            access_token: tokenResponse.access_token,
+          }
+        );
+        console.log(response.data);
+      } catch (error) {
+        console.error("Failed to login with Google:", error);
+      }
+    },
   });
 
   return (
