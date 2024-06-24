1606094a35ef0608017bb400cc8dec434b9fa362
otohusan <tamioguitar@gmail.com>
2024-06-24 12:15:09 +0900

    feat: ダークモードhoookを作成

---
+++ src/hooks/useDarkMode.ts
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+const useDarkMode = () => {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(() => {
+    const savedMode = localStorage.getItem("darkMode");
+    return savedMode ? JSON.parse(savedMode) : false;
+  });
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark-mode");
+    } else {
+      document.documentElement.classList.remove("dark-mode");
+    }
+    localStorage.setItem("darkMode", JSON.stringify(isDarkMode));
+  }, [isDarkMode]);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
+  return [isDarkMode, toggleDarkMode] as const;
+};
+
+export default useDarkMode;
