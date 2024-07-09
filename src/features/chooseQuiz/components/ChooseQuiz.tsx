b3f40a09837a7231c5a99dd4b3345fb679fce560
otohusan <tamioguitar@gmail.com>
2024-07-10 08:59:54 +0900

    style: サイズ感の調整

--- src/features/chooseQuiz/components/ChooseQuiz.tsx
+++ src/features/chooseQuiz/components/ChooseQuiz.tsx
@@ -134,8 +134,8 @@ function ChooseQuiz() {
           />
         )}
 
-        <div className="ChooseTopTitle">学習セット</div>
-        <div className="hr-line"></div>
+        {/* <div className="ChooseTopTitle">学習セット</div>
+        <div className="hr-line"></div> */}
 
         {user && (!data || data?.length === 0) && (
           <p className="choose-quiz-make-prompt">
