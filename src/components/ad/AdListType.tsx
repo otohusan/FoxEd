d6e5b7c960e206860ef0bb398b9d2a90850181d1
otohusan <tamioguitar@gmail.com>
2024-07-16 08:03:57 +0900

    feat: リスト型の広告を作成

---
+++ src/components/ad/AdListType.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef } from "react";
+import "./style/AdAboveNavBar.css";
+
+const AdListType: React.FC = () => {
+  const adRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const iframe = document.createElement("iframe");
+    iframe.style.width = "330px";
+    iframe.style.border = "none";
+    // iframe.style.height = "auto";
+
+    const currentAdRef = adRef.current;
+    if (currentAdRef) {
+      currentAdRef.appendChild(iframe);
+    }
+
+    const htmlContent = `
+      
+      <body>
+        <script src="https://adm.shinobi.jp/s/ef4ab80ca32222c17d761ff785c64643"></script>
+      </body>
+      
+    `;
+
+    iframe.srcdoc = htmlContent;
+
+    return () => {
+      if (currentAdRef) {
+        currentAdRef.removeChild(iframe);
+      }
+    };
+  }, []);
+
+  return (
+    <div className="AdListType-container">
+      <center>
+        <div ref={adRef} />
+      </center>
+    </div>
+  );
+};
+
+export default AdListType;
