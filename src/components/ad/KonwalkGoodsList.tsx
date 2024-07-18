a2f4f81135858b5a969431c46f0593dfd3d89cee
otohusan <tamioguitar@gmail.com>
2024-07-18 10:38:58 +0900

    feat: 宣伝コンポーネントを完成

--- src/components/ad/KonwalkGoodsList.tsx
+++ src/components/ad/KonwalkGoodsList.tsx
@@ -11,12 +11,17 @@ function KonwalkGoodsList() {
       <iframe
         height="162"
         width="375"
-        src="https://suzuri.jp/Konwalk/16246414/five-panel-cap/m/navy/embed"
+        src="https://suzuri.jp/Konwalk/16259443/sacoche/m/natural/embed"
       ></iframe>
       <iframe
         height="162"
         width="375"
-        src="https://suzuri.jp/Konwalk/16246414/five-panel-cap/m/navy/embed"
+        src="https://suzuri.jp/Konwalk/16246414/one-point-t-shirt/l/white/embed"
+      ></iframe>
+      <iframe
+        height="162"
+        width="375"
+        src="https://suzuri.jp/Konwalk/16259296/backpack/free/navy/embed"
       ></iframe>
     </div>
   );
