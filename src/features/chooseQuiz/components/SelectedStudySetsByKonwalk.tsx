4dbb4fec18c31b2f7c5e1b6d313b8f478de3e6f2
otohusan <tamioguitar@gmail.com>
2024-07-21 14:26:13 +0900

    fix: selectしたのが毎回際レンダーされる問題を修正

--- src/features/chooseQuiz/components/SelectedStudySetsByKonwalk.tsx
+++ src/features/chooseQuiz/components/SelectedStudySetsByKonwalk.tsx
@@ -1,6 +1,7 @@
+import React, { useEffect, useState } from "react";
 import { StudySet, User } from "../../../../type";
-import { useFetch } from "../../../hooks";
 import StudySetOverview from "./StudySetOverview";
+import axios from "axios";
 
 type SelectedStudySetsByKonwalkProps = {
   handleClickStudySet: (studyset: StudySet) => void;
@@ -11,6 +12,9 @@ type SelectedStudySetsByKonwalkProps = {
 type SelectedStudySetsByKonwalkContainerProps = {
   searchTerm: string;
   categoryTitle: string;
+  handleClickStudySet: (studyset: StudySet) => void;
+  handleClickMenu: (e: React.MouseEvent<Element, MouseEvent>) => void;
+  user: User | null;
 };
 
 function SelectedStudySetsByKonwalk({
@@ -23,41 +27,68 @@ function SelectedStudySetsByKonwalk({
       <SelectedStudySetsByKonwalkContainer
         searchTerm="IT"
         categoryTitle="IT用語"
+        handleClickStudySet={handleClickStudySet}
+        handleClickMenu={handleClickMenu}
+        user={user}
       />
       <SelectedStudySetsByKonwalkContainer
         searchTerm="日本史"
         categoryTitle="日本史"
+        handleClickStudySet={handleClickStudySet}
+        handleClickMenu={handleClickMenu}
+        user={user}
       />
       <SelectedStudySetsByKonwalkContainer
         searchTerm="世界史"
         categoryTitle="世界史"
+        handleClickStudySet={handleClickStudySet}
+        handleClickMenu={handleClickMenu}
+        user={user}
       />
     </div>
   );
+}
 
-  // handleあたりのpropsを毎回渡すのが煩わしいから、この中で定義してる
-  function SelectedStudySetsByKonwalkContainer({
-    searchTerm,
-    categoryTitle,
-  }: SelectedStudySetsByKonwalkContainerProps) {
-    const BASE_BACKEND_URL = import.meta.env.VITE_BASE_BACKEND_URL;
-    const url = `${BASE_BACKEND_URL}/studysets/search?keyword=${searchTerm}`;
-    const response = useFetch<StudySet[]>(url);
+// TODO: 更新や削除をした場合に、際レンダーされず、すぐに反映されない
+function SelectedStudySetsByKonwalkContainer({
+  searchTerm,
+  categoryTitle,
+  handleClickStudySet,
+  handleClickMenu,
+  user,
+}: SelectedStudySetsByKonwalkContainerProps) {
+  const [studySets, setStudySets] = useState<StudySet[] | null>(null);
+  const BASE_BACKEND_URL = import.meta.env.VITE_BASE_BACKEND_URL;
 
-    return (
-      <div>
-        {response.data && response.data.length > 0 && (
-          <StudySetOverview
-            title={categoryTitle}
-            studySets={response.data}
-            user={user || null}
-            handleClickStudySet={handleClickStudySet}
-            handleClickMenu={handleClickMenu}
-          />
-        )}
-      </div>
-    );
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          `${BASE_BACKEND_URL}/studysets/search?keyword=${searchTerm}`
+        );
+        const data = await response.data;
+        setStudySets(data);
+      } catch (error) {
+        console.error("Error fetching study sets:", error);
+      }
+    };
+
+    fetchData();
+  }, [searchTerm, BASE_BACKEND_URL]);
+
+  return (
+    <div>
+      {studySets && studySets.length > 0 && (
+        <StudySetOverview
+          title={categoryTitle}
+          studySets={studySets}
+          user={user || null}
+          handleClickStudySet={handleClickStudySet}
+          handleClickMenu={handleClickMenu}
+        />
+      )}
+    </div>
+  );
 }
 
 export default SelectedStudySetsByKonwalk;
